0159333902f60a07075aebc896eca9da3e6f3a22
iiborn <abbasgit91@gmail.com>
2017-02-16 17:44:02 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
     "plugins",
     "tools"
  },
-    sudo_users = {157059515},
+    sudo_users = {326824638,180122599},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
