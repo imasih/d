1ed032cd131f595fb466fef97b653c47406890e3
iiborn <abbasgit91@gmail.com>
2017-02-16 18:45:40 +0330

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -4,7 +4,7 @@ local function run(msg, matches)
 if matches[1] == 'kickme' then
 local hash = 'kick:'..msg.chat_id_..':'..msg.sender_user_id_
      redis:set(hash, "waite")
-      return '??کاربر عزيز??\nشما درخواست اخراج خود از گروه را ارسال کرديد\nاگر با اين درخواست موافقت داريد عبارت yes را ارسال کنيد?'
+      return 'کاربر عزيز\nشما درخواست اخراج خود از گروه را ارسال کرديد\nاگر با اين درخواست موافقت داريد عبارت yes را ارسال کنيد'
     end
 msg.text = msg.content_.text_
     if msg.text then
@@ -12,7 +12,7 @@ msg.text = msg.content_.text_
      if msg.text:match("^yes$") and redis:get(hash) == "waite" then
 	  redis:set(hash, "ok")
 	elseif msg.text:match("^no$") and redis:get(hash) == "waite" then
-	tdcli.sendMessage(msg.chat_id_, 0, 1, 'کرم داري؟', 1, 'md')
+	tdcli.sendMessage(msg.chat_id_, 0, 1, 'کنسل شد`', 1`, 'md')
 	  redis:del(hash, true)
       end
     end
@@ -20,7 +20,7 @@ msg.text = msg.content_.text_
 	 if redis:get(hash) then
         if redis:get(hash) == "ok" then
 		     kick_user(msg.sender_user_id_,msg.chat_id_) 
-         return '?کاربر مورد نظر بنابر درخواست خود از گروه اخراج شد??'
+         return 'کاربر مورد نظر بنابر درخواست خود از گروه اخراج شد'
         end
       end
     end
